8bc158ca3310833717f44dbd890f43d7cf4c82eb
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-12-14 16:58:41 +0530

    markers added to map

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require("express");
-const app = express();
+const socketio = require("socket.io");
 const http = require("http");
 const path = require("path");
-const socketio = require("socket.io");
 
+const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  res.send("Hey!");
+io.on("connection", function (socket) {
+  console.log("connected");
+  socket.on("send-location", function (data) {
+    io.emit("recive-location", {
+      id: socket.id,
+      ...data,
+    });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
-app.listen(3000, () => {
-  console.log("app is listening on port 3000 ");
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server Start");
 });
